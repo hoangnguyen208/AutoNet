3642bd32c88c59455152759397344727911dc071
Hoang Nguyen <hoangnguyen.vn208@gmail.com>
2023-11-01 23:26:57 +0200

    move all projects into docker

--- src/SearchService/Program.cs
+++ src/SearchService/Program.cs
@@ -11,13 +11,18 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
 builder.Services.AddHttpClient<AuctionSvcHttpClient>().AddPolicyHandler(GetPolicy());
-builder.Services.AddMassTransit(x => 
+builder.Services.AddMassTransit(x =>
 {
     x.AddConsumersFromNamespaceContaining<AuctionCreatedConsumer>();
     x.SetEndpointNameFormatter(new KebabCaseEndpointNameFormatter("search", false));
-    x.UsingRabbitMq((context, cfg) => 
+    x.UsingRabbitMq((context, cfg) =>
     {
-        cfg.ReceiveEndpoint("search-auction-created", e => 
+        cfg.Host(builder.Configuration["RabbitMq:Host"], "/", host =>
+        {
+            host.Username(builder.Configuration.GetValue("RabbitMq:Username", "guest"));
+            host.Password(builder.Configuration.GetValue("RabbitMq:Username", "guest"));
+        });
+        cfg.ReceiveEndpoint("search-auction-created", e =>
         {
             e.UseMessageRetry(r => r.Interval(5, 5));
             e.ConfigureConsumer<AuctionCreatedConsumer>(context);
@@ -39,7 +44,7 @@ app.Lifetime.ApplicationStarted.Register(async () =>
 {
     try
     {
-        await DbInitializer.InitDb(app);   
+        await DbInitializer.InitDb(app);
     }
     catch (Exception e)
     {
