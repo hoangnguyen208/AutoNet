9480b2c95fdfecf1798f954cedf617cfb72bf3c8
Hoang Nguyen <hoangnguyen.vn208@gmail.com>
2023-11-12 21:29:36 +0200

    wrap up UI

--- frontend/web-app/app/auctions/details/[id]/BidList.tsx
+++ frontend/web-app/app/auctions/details/[id]/BidList.tsx
@@ -20,7 +20,10 @@ export default function BidList({ user, auction }: Props) {
     const [loading, setLoading] = useState(true);
     const bids = useBidStore(state => state.bids);
     const setBids = useBidStore(state => state.setBids);
-    const highBid = bids.reduce((prev, current) => prev > current.amount ? prev : current.amount, 0);
+    const highBid = bids.reduce((prev, current) => prev > current.amount ? prev : current.bidStatus.includes('Accepted') ? current.amount : prev, 0);
+    const open = useBidStore(state => state.open);
+    const setOpen = useBidStore(state => state.setOpen);
+    const openForBids = new Date(auction.auctionEnd) > new Date();
 
     useEffect(() => {
         getBidsForAuction(auction.id)
@@ -35,6 +38,10 @@ export default function BidList({ user, auction }: Props) {
             })
             .finally(() => setLoading(false));
     }, [auction.id, setLoading, setBids])
+
+    useEffect(() => {
+        setOpen(openForBids);
+    }, [openForBids, setOpen])
 
     if (loading) return <span>Loading...</span>
 
@@ -59,17 +66,23 @@ export default function BidList({ user, auction }: Props) {
             </div>
 
             <div className='px-2 pb-2 text-gray-500'>
-                {!user ? (
-                    <div className='flex items-center justify-center p-2 text-lg font-semibold'>
-                        Please login to make a bid
-                    </div>
-                ) : user && user.username === auction.seller ? (
-                    <div className='flex items-center justify-center p-2 text-lg font-semibold'>
-                        You cannot bid on your own auction
-                    </div>
-                ): (
-                    <BidForm auctionId={auction.id} highBid={highBid} />
-                )}
+                {
+                    !open ? (
+                        <div className='flex items-center justify-center p-2 text-lg font-semibold'>
+                            This auction has finished
+                        </div>
+                    ) : !user ? (
+                        <div className='flex items-center justify-center p-2 text-lg font-semibold'>
+                            Please login to make a bid
+                        </div>
+                    ) : user && user.username === auction.seller ? (
+                        <div className='flex items-center justify-center p-2 text-lg font-semibold'>
+                            You cannot bid on your own auction
+                        </div>
+                    ) : (
+                        <BidForm auctionId={auction.id} highBid={highBid} />
+                    )  
+                }
             </div>
         </div>
     )
--- frontend/web-app/app/providers/SignalRProvider.tsx
+++ frontend/web-app/app/providers/SignalRProvider.tsx
@@ -2,15 +2,21 @@
 
 import { useAuctionStore } from '@/hooks/useAuctionStore';
 import { useBidStore } from '@/hooks/useBidStore';
-import { Bid } from '@/types';
+import { Auction, AuctionFinished, Bid } from '@/types';
 import { HubConnection, HubConnectionBuilder } from '@microsoft/signalr'
+import { User } from 'next-auth';
 import React, { ReactNode, useEffect, useState } from 'react'
+import toast from 'react-hot-toast';
+import AuctionCreatedToast from '../components/AuctionCreatedToast';
+import { getDetailedViewData } from '../actions/auctionActions';
+import AuctionFinishedToast from '../components/AuctionFinishedToast';
 
 type Props = {
-    children: ReactNode
+    children: ReactNode,
+    user: User | null
 }
 
-export default function SignalRProvider({ children }: Props) {
+export default function SignalRProvider({ children, user }: Props) {
     const [connection, setConnection] = useState<HubConnection | null>(null);
     const setCurrentPrice = useAuctionStore(state => state.setCurrentPrice);
     const addBid = useBidStore(state => state.addBid);
@@ -28,12 +34,26 @@ export default function SignalRProvider({ children }: Props) {
         if (connection) {
             connection.start()
                 .then(() => {
-                    console.log('Connected to notification hub');
                     connection.on('BidPlaced', (bid: Bid) => {
-                        console.log('Bid placed event received');
                         if (bid.bidStatus.includes('Accepted')) {
                             setCurrentPrice(bid.auctionId, bid.amount);
                         }
+                        addBid(bid);
+                    });
+
+                    connection.on('AuctionCreated', (auction: Auction) => {
+                        if (user?.username !== auction.seller) {
+                            return toast(<AuctionCreatedToast auction={auction} />, {duration: 10000});
+                        }
+                    })
+
+                    connection.on('AuctionFinished', (finishedAuction: AuctionFinished) => {
+                        const auction = getDetailedViewData(finishedAuction.auctionId);
+                        return toast.promise(auction, {
+                            loading: 'Loading',
+                            success: (auction) => <AuctionFinishedToast finishedAuction={finishedAuction} auction={auction} />,
+                            error: (err) => 'Auction finished!'
+                        }, {success: {duration: 10000, icon: null}})
                     })
                 }).catch(error => console.log(error));
         }
